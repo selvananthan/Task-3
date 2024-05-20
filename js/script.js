8238c7320e4685191ec89d70a8b32556485b8806
selvananthan <selvagoogly@gmail.com>
2024-05-20 13:46:04 +0530

    Task 3

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function reqListener() {
+    
+    console.log(this.responseText);
+    const arr=JSON.parse(this.responseText);
+    console.log(arr);
+    for (let i=0;i<arr.length; i++){
+
+console.log(arr[i].flags.png);
+console.log(arr[i].name.common);
+console.log(arr[i].region);
+console.log(arr[i].subregion);
+console.log(arr[i].population);
+    }
+  }
+  
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://restcountries.com/v3.1/all");
+  req.send();
+  
